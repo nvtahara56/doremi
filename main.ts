11bf86d0f3a8ed5af4b4d20cabf622b0ed4fa010
nvtahara56 <52019961+nvtahara56@users.noreply.github.com>
2021-11-11 12:00:40 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,11 @@
+music.playMelody("C C C D E E C C ", 240)
+music.playMelody("E E C C D D D - ", 240)
+music.playMelody("D D D E F F E D ", 240)
+music.playMelody("F F F F F F - - ", 240)
+music.playMelody("E E E F G G E E ", 240)
+music.playMelody("G G E E G G G - ", 240)
+music.playMelody("F F F G A A G F ", 240)
+music.playMelody("A A A A A A - - ", 240)
+basic.forever(function () {
+	
+})
